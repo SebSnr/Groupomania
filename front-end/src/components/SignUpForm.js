f921f61444f5add23ed11f07aeebaa73f7ff6328
Seb <sanner.sebastien@gmail.com>
2021-06-25 10:58:05 +0200

    :lock: validate input form signup

--- front-end/src/components/SignUpForm.js
+++ front-end/src/components/SignUpForm.js
@@ -1,13 +1,18 @@
 import React from "react"
-import {Formik, Form, Field} from "formik"
+import {Formik, Form, Field, ErrorMessage} from "formik"
 import * as Yup from "yup"
 
-export default function LoginForm() {
+export default function LoginForm() { 
+
 	const SignupSchema = Yup.object().shape({
 		firstName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
 		lastName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
 		email: Yup.string().email("Invalid email").required("Required"),
-	})
+		photo: Yup.mixed()
+			.test('fileSize', "File Size is too large", (value) => value === null || (value && value.size <= 2000000))
+			.test('fileType', "Unsupported File Format", value => value && ['image/jpg', 'image/jpeg', 'image/gif', 'image/png'].includes(value.type) )
+			.required("required"),
+})
 
 	return (
 		<div>
@@ -17,6 +22,7 @@ export default function LoginForm() {
 					firstName: "",
 					lastName: "",
 					email: "",
+					photo: null,
 				}}
 				validationSchema={SignupSchema}
 				onSubmit={(values) => {
@@ -24,18 +30,27 @@ export default function LoginForm() {
 					console.log(values)
 				}}
 			>
-				{({errors, touched}) => (
-					<Form>
-						<Field name="firstName" />
-						{errors.firstName && touched.firstName ? <div>{errors.firstName}</div> : null}
-						<Field name="lastName" />
-						{errors.lastName && touched.lastName ? <div>{errors.lastName}</div> : null}
-						<Field name="email" type="email" />
-						{errors.email && touched.email ? <div>{errors.email}</div> : null}
-						<button type="submit">Submit</button>
-					</Form>
-				)}
+				<Form>
+					<label htmlFor="firstName">Prénom</label>
+					<Field name="firstName" type="text" placeholder="Marie" />
+					<ErrorMessage name="firstName" />
+
+					<label htmlFor="lastName">Nom</label>
+					<Field name="lastName" type="text" placeholder="Dupont" />
+					<ErrorMessage name="lastName" />
+
+					<label htmlFor="email">Email</label>
+					<Field name="email" type="email" placeholder="marie.dupont@gmail.com" />
+					<ErrorMessage name="email" />
+
+					<label htmlFor="photo">Photo de profil</label>
+					<Field name="photo" type="file" accept=".jpg, .jpeg, .png"/>
+
+					<button type="submit">M'inscrire</button>
+				</Form>
 			</Formik>
 		</div>
 	)
 }
+
+
--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react';
+import React from 'react';
 import Connexion from '../components/Connexion';
 import SocialBar from '../components/SocialBar';
 
