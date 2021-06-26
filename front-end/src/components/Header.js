d2f185c49fd1842310f6df95be6f4669f20ba0b8
Seb <sanner.sebastien@gmail.com>
2021-06-26 17:36:39 +0200

    :art: start home page & header

---
+++ front-end/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header >
+            <img src='/img/icon-left-font.svg' />
+                <nav>
+                    <ul>
+                        <li>Membres</li>
+                        <li>Profile</li>
+                    </ul>
+                </nav>
+            header
+        </header>
+    )
+}
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import HomeContent from '../components/HomeContent'
+
 
 export default function Home() {
 
     return (
-        <div>
-            Bonjour Home
+        <div className="homeContainer">
+            <Header />
+            <HomeContent />
+            <Footer />
         </div>
-        
-
     )
 
 }
