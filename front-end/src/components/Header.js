54eef4254cab8676e232cbc73a378148fefdfa94
Seb <sanner.sebastien@gmail.com>
2021-06-27 20:15:45 +0200

    :lipstick: do toggle menu

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,24 +1,33 @@
-import React from 'react'
+import React from "react"
 
 export default function Header() {
-    return (
-            <header className="main-header" >
-                <div className="main-header__content container" >
+	return (
+		<header className="header">
+			<div className="header__content container">
+				<nav className="navigation">
 
-                        <nav className="main-navigation">
-                            <div className="nav-wrapper">
+					<img src="/img/icon.png" alt="Bouton retour accueil" />
 
-                                <img src='/img/icon.png' alt="Bouton retour accueil"/>
+					<div>
+						<input type="checkbox" id="menu-checkbox" class="menu-checkbox" />
+						<label for="menu-checkbox" class="menu-toggle">
+							<span></span>
+							<span></span>
+							<span></span>
+						</label>
 
-                                <input type="checkbox" id="menu-checkbox" class="menu-checkbox" />
-                                <label for="menu-checkbox" class="menu-toggle">≡ Menu</label>
-                                <ul className="menu">
-                                    <li><a href="b">Membres</a></li>
-                                    <li><a href="b">Profile</a></li>
-                                </ul>
-                            </div>
-                        </nav>
-                </div>
-            </header>
-    )
+						<ul className="menu">
+							<li>
+								<a href="b">Membres</a>
+							</li>
+							<li>
+								<a href="b">Profile</a>
+							</li>
+						</ul>
+					</div>
+                    
+				</nav>
+			</div>
+		</header>
+	)
 }
