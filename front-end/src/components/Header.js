3af5629737b0b6e99c6614461a8a78e96acf634b
Seb <sanner.sebastien@gmail.com>
2021-06-28 11:48:42 +0200

    :lipstick: update header

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -3,31 +3,29 @@ import React from "react"
 export default function Header() {
 	return (
 		<header className="header">
-			<div className="header__content container">
-				<nav className="navigation">
+			<nav className="navigation container">
+				<a href="/">
+					<img src="/img/icon.png" alt="Bouton retour accueil" />
+				</a>
 
-					<a href="/" ><img src="/img/icon.png" alt="Bouton retour accueil" /></a>
+				<div className="menu-content">
+					<input type="checkbox" id="menu-checkbox" class="menu-checkbox" />
+					<label for="menu-checkbox" class="menu-toggle">
+						<span></span>
+						<span></span>
+						<span></span>
+					</label>
 
-					<div className="menu-content">
-						<input type="checkbox" id="menu-checkbox" class="menu-checkbox" />
-						<label for="menu-checkbox" class="menu-toggle">
-							<span></span>
-							<span></span>
-							<span></span>
-						</label>
-
-						<ul className="menu">
-							<li>
-								<a href="b">Membres</a>
-							</li>
-							<li>
-								<a href="b">Profile</a>
-							</li>
-						</ul>
-					</div>
-                    
-				</nav>
-			</div>
+					<ul className="menu">
+						<li>
+							<a href="b">Membres</a>
+						</li>
+						<li>
+							<a href="b">Profile</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
 		</header>
 	)
 }
