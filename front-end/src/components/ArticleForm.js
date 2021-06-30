6306d44ad1246d041a3ccc895c6a84c9defd9d59
Seb <sanner.sebastien@gmail.com>
2021-06-30 13:54:26 +0200

    :art: start axios post

--- front-end/src/components/ArticleForm.js
+++ front-end/src/components/ArticleForm.js
@@ -1,22 +1,35 @@
 import React from "react"
 import {Formik, Form, Field, ErrorMessage} from "formik"
 import * as Yup from "yup"
+import axios from "axios"
 
 export default function ArticleForm() {
 	const SignupSchema = Yup.object().shape({
 		articleBody: Yup.string().min(2, "trop court*").max(300, "Maximum 300 charactères*").required("obligatoire*"),
-		articlePicture: Yup.mixed()
-			// .test("fileSize", "photo trop lourde", (value) => value === null || (value && value.size <= 2000000))
-			// .test("fileType", "formats autorisés : jpg, jpeg, png", (value) => value && ["image/jpg", "image/jpeg", "image/png"].includes(value.type))
-			.required("obligatoire*"),
+		articlePicture: Yup.mixed(),
+		// .test("fileSize", "photo trop lourde", (value) => value === null || (value && value.size <= 2000000))
+		// .test("fileType", "formats autorisés : jpg, jpeg, png", (value) => value && ["image/jpg", "image/jpeg", "image/png"].includes(value.type))
+		// .required("obligatoire*"),
 	})
 
+	function handleEditArticle(values) {
+		// values.preventDefault()
+		
+		let articleBody = values.articleBody
+		// let articlePicture = values.articlePicture
 
+		
+		console.log(values)
 
+		axios
+			.post("http://localhost:3002/articles", {
+				articleBody
+			})
+			.then(() => console.log("Post créé"))
+			.catch((err) => console.log(err))
 
-
-
-    
+		// window.location.reload()
+	}
 
 	return (
 		<div className="card articleForm mb-5">
@@ -27,8 +40,8 @@ export default function ArticleForm() {
 				}}
 				validationSchema={SignupSchema}
 				onSubmit={(values) => {
-					// same shape as initial values
-					console.log(values)
+					handleEditArticle(values)
+					console.log(values.articleBody)
 				}}
 			>
 				<Form>
