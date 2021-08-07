cffa22bda17cf9e2ef47bb23008970591531d081
Seb <sanner.sebastien@gmail.com>
2021-08-07 16:45:40 +0200

    :truck: rename fakeData

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const helmet = require("helmet")
 const cookieSession = require("cookie-session")
-const fakeData = require("./test")
+const fakeData = require("./config/fakeData")
 
 const app = express()
 
--- back-end/config/fakeData.js
+++ back-end/config/fakeData.js
@@ -1,4 +1,4 @@
-const db = require("./models")
+const db = require("../models")
 const User = db.User
 const Article = db.Article
 const Comment = db.Comment
